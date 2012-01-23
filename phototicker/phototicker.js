bd765e140c44e628919916f615ebf205e559538e
doug <doug@meetup.com>
2012-01-23 10:29:36 -0500

    tidying up, add photo filter

--- phototicker/phototicker.js
+++ phototicker/phototicker.js
@@ -1,41 +1,82 @@
 (function($, mu) {
+ if (!Array.prototype.filter){
+    Array.prototype.filter = function(fun){
+        var len = this.length;
+        if (typeof fun != "function") { throw new TypeError(); }
+        var res = [], thisp = arguments[1];
+        for (var i = 0; i < len; i++) {
+            if (i in this) {
+                var val = this[i];
+                if (fun.call(thisp, val, i, this)) { res.push(val); }
+            }
+        }
+        return res;
+    };
+ }
+ if (!Array.prototype.indexOf) {
+    Array.prototype.indexOf = function(elt) {
+        var len = this.length, from = Number(arguments[1]) || 0;
+        from = (from < 0)
+            ? Math.ceil(from)
+            : Math.floor(from);
+        if (from < 0) from += len;
+
+        for (; from < len; from++) {
+            if (from in this && this[from] === elt) return from;
+        }
+        return -1;
+    };
+ }
  $(function() {
-    var content = $("#content")      
+   var content = $("#content")
+   , blacklist = [
+       'naturism',
+       ,'figuredrawing',
+       ,'beautyindustry',
+       ,'modeling',
+       ,'model-photography',
+       ,'studio-photography'
+    ]
+    , blacklisted = function(topic) {
+       return blacklist.indexOf(topic.urlkey) !== -1; 
+    }
     , queue = []
     , enqueue = function(p) {
-        queue.push(p);
+        var topics = p.photo_album.group.group_topics;
+        if(!topics || topics.filter(blacklisted).length < 1) {
+          queue.push(p);
+        }
     }
     , dequeue = function() {
         return queue.shift();
     }
-    , pollInterval = 4000
-	, maxPhotos = 10
+    , pollInterval = 5000
+    , maxPhotos = 2
     , img = function(src) {
         return '<img src="'+src+'"/>';
     }
     , render = function(p) {
-        return ['<li><div class="p">'
-                , img(p.highres_link)
-                ,'</div></li>'].join('');
+        return '<li><div class="p">' + img(p.highres_link) + '</div></li>';
     }
     , process = function(p) {
       if(p.photo_album.event) {
-        var el = $(render(p)), photos = content.children(), psize = photos.size();
-        el.hide();
-		photos.each(function(i) {
-          if (i < psize) $(this).remove();
+        var el = $(render(p))
+          , photos = content.children()
+          , psize = photos.size();
+        photos.each(function(i) {
+          if (i < psize - maxPhotos) $(this).remove();
         });
+        el.hide();
         content.prepend(el);
-		$('div.caption').slideUp();
-        el.find('img').load(function(){
-          el.fadeIn('slow');
-          var pa = p.photo_album
-          , group = pa.group
-		  , event = pa.event;
-		  $('div.caption').slideDown();
-		  $('div.group').html(group.name);
-		  $('div.event').html(event.name);
-		});
+        $('#caption').slideUp('fast');
+        el.find('.p img').load(function() {
+          el.animate({ height: 'show' }, 700, function() {
+            var pa = p.photo_album;
+            $('#group').html(pa.group.name);
+            $('#event').html(pa.event.name);
+            $('#caption').slideDown('slow');            
+          });          
+        });
       }
     }
     , pollq = function() {
