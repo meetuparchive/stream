e6ad860404a7a11c4a8b07e9973d740da2ed34bc
softprops <d.tangren@gmail.com>
2022-02-07 01:39:21 -0500

    retire more

--- js/arrays.js
+++
@@ -1,5 +0,0 @@
-if(!Array.prototype.shuffle) {
-  Array.prototype.shuffle = function (){
-     for(var rnd, tmp, i=this.length; i; rnd=parseInt(Math.random()*i), tmp=this[--i], this[i]=this[rnd], this[rnd]=tmp);
-  };
-}
